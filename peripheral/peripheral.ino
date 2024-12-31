437958a4f5e67940b846c39596dd579ea356e95b
Amer <amusinbego1@etf.unsa.ba>
2024-12-31 19:14:04 +0100

    make built_in led as relay output

--- peripheral/peripheral.ino
+++ peripheral/peripheral.ino
@@ -4,12 +4,10 @@
 const char *passkey = "123456";
 bool authenticated = false;
 
-BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
 BLEService sensorService("5a005939-6dad-4166-9531-2d8d363a462c");
 BLEService authService("9964e111-9289-4507-b935-c321bea0afbe");
 
 // Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
-BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 BLEStringCharacteristic temperatureCharacteristic("91a0b53d-0624-4b15-b388-59afcf03f233", BLERead, 8);
 BLEStringCharacteristic humidityCharacteristic("89c028a0-d1bf-4f8f-97d6-3fa8c77fdcf7", BLERead, 8);
 BLEStringCharacteristic passkeyCharacteristic("30ead979-dd63-4fe5-a2ca-e76ae9ce0c9c", BLEWrite, 8);
@@ -35,24 +33,21 @@ void setup() {
 
     // set advertised local name and service UUID:
   BLE.setLocalName("Health");
-  BLE.setAdvertisedService(ledService);
   BLE.setAdvertisedService(sensorService);
   BLE.setAdvertisedService(authService);
 
   // add the characteristic to the service
-  ledService.addCharacteristic(switchCharacteristic);
   sensorService.addCharacteristic(temperatureCharacteristic);
   sensorService.addCharacteristic(humidityCharacteristic);
   authService.addCharacteristic(passkeyCharacteristic);
 
   // add service
-  BLE.addService(ledService);
   BLE.addService(sensorService);
   BLE.addService(authService);
 
   // set the initial value for the characeristic:
-  switchCharacteristic.writeValue(0);
   passkeyCharacteristic.writeValue("");
+  digitalWrite(ledPin, LOW);
 
   // start advertising
   BLE.advertise();
@@ -84,16 +79,7 @@ void loop() {
         if(authenticated){
           temperatureCharacteristic.writeValue(String(HTS.readTemperature()));
           humidityCharacteristic.writeValue(String(HTS.readHumidity()));
-
-          if (switchCharacteristic.written()) {
-            if (switchCharacteristic.value()) {
-              Serial.println("LED on");
-              digitalWrite(ledPin, HIGH);
-            } else {
-              Serial.println("LED off");
-              digitalWrite(ledPin, LOW);
-            }
-          }
+          digitalWrite(ledPin, HIGH);
         } else {
           digitalWrite(ledPin, LOW);
           temperatureCharacteristic.writeValue("No Value");
@@ -103,6 +89,7 @@ void loop() {
       }
     }
     authenticated = false;
+    digitalWrite(ledPin, LOW);
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
   }
